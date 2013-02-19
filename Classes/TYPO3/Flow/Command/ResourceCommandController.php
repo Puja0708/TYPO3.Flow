c2edb6160081cad7230c53388df8e2ab94ed71e4
Robert Lemke <robert@typo3.org>
2013-02-19 13:29:55 +0100

    [FEATURE] resource:publishstatic command
    Change-Id: Ic306961d161dfcb6862e445e3c7f9f73b3109cc8

---
+++ Classes/TYPO3/Flow/Command/ResourceCommandController.php
@@ -0,0 +1,59 @@
+<?php
+namespace TYPO3\Flow\Command;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow framework.                       *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Cli\CommandController;
+
+/**
+ * Resource command controller for the TYPO3.Flow package
+ *
+ * @Flow\Scope("singleton")
+ */
+class ResourceCommandController extends CommandController {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Package\PackageManagerInterface
+	 */
+	protected $packageManager;
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Resource\Publishing\ResourcePublisher
+	 */
+	protected $resourcePublisher;
+
+	/**
+	 * Publish static resources
+	 *
+	 * This command triggers publication of static assets of all packages to the
+	 * configured publishing targets.
+	 *
+	 * @param string $package Only publish static resources of this package
+	 * @return void
+	 */
+	public function publishStaticCommand($package = NULL) {
+		if ($package !== NULL) {
+			$package = $this->packageManager->getPackage($package);
+			$this->outputLine($package->getPackageKey());
+			$this->resourcePublisher->publishStaticResources($package->getResourcesPath() . 'Public/', $package->getPackageKey());
+		} else {
+			foreach ($this->packageManager->getActivePackages() as $package) {
+				$this->outputLine($package->getPackageKey());
+				$this->resourcePublisher->publishStaticResources($package->getResourcesPath() . 'Public/', $package->getPackageKey());
+			}
+		}
+	}
+
+}
+?>
